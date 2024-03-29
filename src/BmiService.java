2adfb0ac2cf4c6b051f7f0e0066c315652194601
Maria Gamzina <gamzinamaria@gmail.com>
2024-03-29 23:24:53 +0300

    change

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate(double height, double weight) {
-        double index = weight/(height*height);
+    public int calculate(double heightMeter, double weightKilogram) {
+        double index = weightKilogram / (heightMeter * heightMeter);
         return (int) index;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double height = 1.87;
-        int weight = 98;
-        int bmi = service.calculate(height, weight);
+        double heightMeter = 1.87;
+        int weightKilogram = 98;
+        int bmi = service.calculate(heightMeter, weightKilogram);
         System.out.println(bmi);
-        }
     }
+}
